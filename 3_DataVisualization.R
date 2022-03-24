c7e0aed0ad224a45e207b4d5701073a2a43b6b64
Meghan K Hatfield <meghankhatfield@Meghans-Air.attlocal.net>
2022-03-24 16:09:15 -0700

    Facets and geoms

--- 3_DataVisualization.R
+++ 3_DataVisualization.R
@@ -109,3 +109,127 @@ ggplot(data = mpg) +
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy)) + 
   facet_grid(.~ cyl)
+
+#3.5.1 Exercises
+#1. What happens when you facet on a continuous variable?
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_wrap(~ hwy, nrow = 2)
+#Each unique value of the continuous variable has its own facet/cell
+
+#2. What do the empty cells in plot with facet_grid(drv ~ cyl) mean? 
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_grid(drv ~ cyl)
+#There there are no values for that stratification
+#How do they relate to this plot?
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = drv, y = cyl))
+#There are points missing for drv=r and cyl=5 or cyl=4
+
+#3. What plots does the following code make? What does . do?
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(drv ~ .)
+#This plot facets on rows
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(. ~ cyl)
+#This plot facets on columns
+
+#4. Take the first faceted plot in this section:
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_wrap(~ class, nrow = 2)
+#What are the advantages to using faceting instead of the colour aesthetic? 
+#You can more clearly see the data points of a particular stratification. May be difficult to properly see
+#if you have many data points that overlap depending on stratification.
+#What are the disadvantages? How might the balance change if you had a larger dataset?
+#This would be difficult to use if you have a variable with many categories as this would mean many facets and
+#could get very busy. 
+
+#5. Read ?facet_wrap.
+?facet_wrap
+#What does nrow do? Number of rows
+#What does ncol do? Number of columns
+#What other options control the layout of the individual panels? dir, drop, strip.position, switch, shrink
+#Why doesn’t facet_grid() have nrow and ncol arguments?
+?facet_grid
+#Since this takes two faceting variables to make a matrix of panels, these arguments would not be useful here.
+
+#3.6 Geometric Objects
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  geom_smooth(mapping = aes(x = displ, y = hwy))
+
+#This code produces same plot as above as this applies these mappings as global mappings
+#Useful to avoid potential error introduced by having to update the graphs in multiple places when a var changes
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point() + 
+  geom_smooth()
+
+#Allows you to add multiple layers to existing plot
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point(mapping = aes(color = class)) + 
+  geom_smooth()
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point(mapping = aes(color = class)) + 
+  geom_smooth(data = filter(mpg, class == "subcompact"), se = FALSE)
+
+#Exercises 3.6.1
+#1. What geom would you use to draw a line chart? #geom_line
+#A boxplot? geom_boxplot
+#A histogram? geom_bar
+#An area chart? geom_area
+
+#2. Run this code in your head and predict what the output will look like. 
+#Then, run the code in R and check your predictions.
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = drv)) + 
+  geom_point() + 
+  geom_smooth(se = FALSE)
+
+
+#3.What does show.legend = FALSE do? What happens if you remove it?
+#This removes the legend. If you remove this, then by default, the legend will appear on the plot.
+
+#4. What does the se argument to geom_smooth() do?
+#This shows the se/confidence intervals around the point
+
+#5. Will these two graphs look different? Why/why not?
+#No, these will not look different.
+#In the first graph you are specifying the global mapping.
+#In the second graph, you specify these mappings manually.
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point() + 
+  geom_smooth()
+
+ggplot() + 
+  geom_point(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_smooth(data = mpg, mapping = aes(x = displ, y = hwy))
+
+#6. Recreate the R code necessary to generate the following graphs.
+ggplot(data=mpg, mapping=aes(x=displ,y=hwy)) +
+  geom_point() +
+  geom_smooth(se=FALSE)
+
+ggplot(data=mpg, mapping=aes(x=displ,y=hwy,class=drv)) +
+  geom_point() +
+  geom_smooth(se=FALSE)
+
+ggplot(data=mpg, mapping=aes(x=displ,y=hwy,color=drv)) +
+  geom_point() +
+  geom_smooth(se=FALSE)
+
+ggplot(data=mpg, mapping=aes(x=displ,y=hwy)) +
+  geom_point(mapping=aes(color=drv)) +
+  geom_smooth(se=FALSE)
+
+ggplot(data=mpg, mapping=aes(x=displ,y=hwy)) +
+  geom_point(mapping=aes(color=drv)) +
+  geom_smooth(mapping=aes(linetype=drv), se=FALSE)
+
+ggplot(data=mpg, mapping=aes(x=displ,y=hwy)) +
+  geom_point(mapping=aes(color=drv, stroke=1)) 
+
+#3.7 Statistical Transformations
